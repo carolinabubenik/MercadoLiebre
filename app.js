cdbb057f654dbf07995b2f8d48e72ee9988e0165
Carolina <carolinabubenik@gmail.com>
2022-10-23 21:29:51 -0300

    Consignas del primer pdf terminadas.

--- app.js
+++ app.js
@@ -2,15 +2,46 @@
 const express = require('express');
 const path = require('path');
 
-// Para administrar el servidor web
-const app = express();
+// Constantes y Variables
+const app = express(); // Para administrar el servidor web
+const puerto = 3001; // Puerto a utilizar para el servidor web
+const url = 'http://localhost'; // Url a utilizar para el servidor web
+
+// Definir como públicos los recursos
+app.use( express.static(path.resolve(__dirname, './public')));
 
 // Levantar servidor web con Express
-const puerto = 3000;
-const url = 'http://localhost';
-app.listen(puerto, () => console.log('Servidor corriendo en ' + url + ':' + puerto + '/'))
+app.listen(puerto, () => console.log('Servidor corriendo en ' + url + ':' + puerto + '/'));
 
 // GETs
 app.get('/', (req, res) => 
-    res.sendFile(path.resolve(__dirname, 'views/index.html'))
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+)
+
+app.get('/ofertas', (req, res) => 
+    res.sendFile(path.resolve(__dirname, './views/ofertas.html'))
+)
+
+app.get('/tiendas-oficiales', (req, res) => 
+    res.sendFile(path.resolve(__dirname, './views/tiendas-oficiales.html'))
+)
+
+app.get('/vender', (req, res) => 
+    res.sendFile(path.resolve(__dirname, './views/vender.html'))
+)
+
+app.get('/ayuda', (req, res) => 
+    res.sendFile(path.resolve(__dirname, './views/ayuda.html'))
+)
+
+app.get('/register', (req, res) => 
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+)
+
+app.get('/login', (req, res) => 
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+)
+
+app.get('/mis-compras', (req, res) => 
+    res.sendFile(path.resolve(__dirname, './views/mis-compras.html'))
 )
