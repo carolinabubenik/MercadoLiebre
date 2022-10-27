ebf044c6a51894c583f31b858d0639803b3a3fd1
Carolina <carolinabubenik@gmail.com>
2022-10-27 20:05:52 -0300

    Puerto con variable de entorno.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Constantes y Variables
 const app = express(); // Para administrar el servidor web
-const puerto = 3001; // Puerto a utilizar para el servidor web
+const puerto = process.env.PORT || 3001; // Puerto a utilizar para el servidor web
 const url = 'http://localhost'; // Url a utilizar para el servidor web
 
 // Definir como públicos los recursos
